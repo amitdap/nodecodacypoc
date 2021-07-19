3782cc678bfb58bb4828a0e9d3c01e7aec603135
amit deshmukh <amit.deshmukh@dev.figmd.com>
2021-07-19 18:41:47 +0530

    Remove createQuery code.

--- business/user.js
+++ business/user.js
@@ -4,7 +4,7 @@ let user = () => { };
 user.getDetails = async (req, res, next) => {
     const pool = new Pool({});
     //// Added SQL injection code.
-    createQuery("select * from bank_accounts where id = '" + inputId + "'");
+    //createQuery("select * from bank_accounts where id = '" + inputId + "'");
 
     const queryText = "SELECT * FROM bank_accounts WHERE dob = '" + req.body.dob + "' AND bank_account = '" + req.body.account_number + "'";
     const values = [];
