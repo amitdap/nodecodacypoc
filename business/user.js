6e05576dd6630023d914fc0552f332d6b71a8bc6
amit deshmukh <amit.deshmukh@dev.figmd.com>
2021-07-19 17:57:28 +0530

    #Added new line for SQL injection.

--- business/user.js
+++ business/user.js
@@ -4,6 +4,8 @@ let user = () => { };
 user.getDetails = async (req, res, next) => {
     const pool = new Pool({});
     //// Added SQL injection code.
+    createQuery("select * from bank_accounts where id = '" + inputId + "'");
+
     const queryText = "SELECT * FROM bank_accounts WHERE dob = '" + req.body.dob + "' AND bank_account = '" + req.body.account_number + "'";
     const values = [];
     const response = await pool.query(queryText, values);
