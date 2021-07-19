9eb5fdf56f16c06819923f48c9dcfcd9f31110d0
amit deshmukh <amit.deshmukh@dev.figmd.com>
2021-07-19 16:18:35 +0530

    Added server.js  and GET endpoint.

---
+++ server.js
@@ -0,0 +1,29 @@
+// include dependencies
+const express = require("express");
+const app = express();
+const http = require("http");
+let cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const cors = require("cors");
+
+app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!');
+});
+
+var server = http.createServer(app);
+server.listen(process.env.NODECODACY_PORT);
+
+
+console.log("app listening on port: " + process.env.NODECODACY_PORT);
+
+module.exports = server;
